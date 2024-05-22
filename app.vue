bdce48b3d2ca65e0b8cee44162a8b54c33ee052c
miguelit0 <meduardofuentesc@gmail.com>
2024-05-22 11:48:37 -0500

    Add purple theme

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  
+    
+    <h1> CineKubo</h1>
+
+    <input type="text">
+  
 </template>
+
+<script>
+import 'primevue/resources/themes/lara-dark-purple/theme.css'
+</script>
